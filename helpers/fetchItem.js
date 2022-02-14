b28d128940f7cc2a6707b40081b45032bd8657a7
VanehSann <vanessacbsantos22@gmail.com>
2022-02-14 13:45:50 -0300

    removendo conflito

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -2,7 +2,7 @@ const fetchItem = async (productItem) => {
   const resultResponse = await fetch(`https://api.mercadolibre.com/items/${productItem}`);
   const results = await resultResponse.json();
 return results;
-}
+};
 
 if (typeof module !== 'undefined') {
   module.exports = {
